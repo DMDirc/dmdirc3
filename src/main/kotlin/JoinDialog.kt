693da7d9a9fe0d9c01834e643a12b6d0c188056d
Greg Holmes <git@greg.holmes.name>
2019-02-05 20:05:22 +0000

    Switch to model for join dialog.

--- src/main/kotlin/JoinDialog.kt
+++ src/main/kotlin/JoinDialog.kt
@@ -1,30 +1,35 @@
 package com.dmdirc
 
+import javafx.scene.control.ButtonBar
 import tornadofx.*
 
-class JoinDialog: Fragment() {
+data class JoinDetails(val channel: String)
+
+class JoinDetailsModel : ItemViewModel<JoinDetails>() {
     private val controller: MainController by inject()
+    val channel = bind(JoinDetails::channel)
+    override fun onCommit() {
+        controller.joinChannel(channel.value)
+    }
+}
+
+class JoinDialog: Fragment() {
+    private val model = JoinDetailsModel()
     override val root = form {
         fieldset {
             field("Channel Name") {
-                textfield(controller.channelName)
+                textfield(model.channel).required()
             }
         }
         buttonbar {
-            button("Connect") {
-                isDefaultButton = true
-                enableWhen {
-                    controller.isConnected.and(controller.channelName.isNotEmpty)
-                }
+            button("Join", ButtonBar.ButtonData.OK_DONE) {
+                enableWhen(model.valid)
                 action {
-                    controller.joinChannel(
-                            name=controller.channelName.value
-                    )
+                    model.commit()
                     close()
                 }
             }
-            button("Cancel") {
-                isCancelButton = true
+            button("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE) {
                 action {
                     close()
                 }
