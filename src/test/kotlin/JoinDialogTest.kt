c45f77f2e001f1cec7ef16fc881ac82e6bb1cfb8
Chris Smith <chris@chameth.com>
2019-03-02 00:07:53 +0000

    Add tests for the join dialog

---
+++ src/test/kotlin/JoinDialogTest.kt
@@ -0,0 +1,103 @@
+
+import com.dmdirc.JoinDialogContract
+import com.dmdirc.JoinDialogController
+import com.dmdirc.JoinDialogModel
+import com.dmdirc.MainContract
+import io.mockk.mockk
+import io.mockk.verify
+import javafx.beans.property.SimpleBooleanProperty
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class JoinDialogControllerTest {
+
+    private val mockMainController = mockk<MainContract.Controller>()
+    private val controller = JoinDialogController(mockMainController)
+
+    @Test
+    fun `calls joinChannel on main controller when joinChannel called`() {
+        controller.join("#test123")
+        verify {
+            mockMainController.joinChannel("#test123")
+        }
+    }
+
+}
+
+internal class JoinDialogModelTest {
+
+    private val mockController = mockk<JoinDialogContract.Controller>()
+    private val truthValidator = SimpleBooleanProperty(true)
+    private val falseValidator = SimpleBooleanProperty(false)
+    private val model = JoinDialogModel(mockController)
+
+    @Test
+    fun `defaults to dialog being open`() {
+        assertTrue(model.open.value)
+    }
+
+    @Test
+    fun `closes when cancel pressed`() {
+        model.onCancelPressed()
+        assertFalse(model.open.value)
+    }
+
+    @Test
+    fun `joins when valid and join pressed`() {
+        model.valid.addValidator(truthValidator)
+        model.channel.set("#dmdirc")
+        model.onJoinPressed()
+        verify {
+            mockController.join("#dmdirc")
+        }
+    }
+
+    @Test
+    fun `closes when valid and join pressed`() {
+        model.valid.addValidator(truthValidator)
+        model.channel.set("#dmdirc")
+        model.onJoinPressed()
+        assertFalse(model.open.value)
+    }
+
+
+    @Test
+    fun `joins when valid and text submitted`() {
+        model.valid.addValidator(truthValidator)
+        model.channel.set("#dmdirc")
+        model.onTextAction()
+        verify {
+            mockController.join("#dmdirc")
+        }
+    }
+
+    @Test
+    fun `closes when valid and text submitted`() {
+        model.valid.addValidator(truthValidator)
+        model.channel.set("#dmdirc")
+        model.onTextAction()
+        assertFalse(model.open.value)
+    }
+
+    @Test
+    fun `does nothing when invalid and join pressed`() {
+        model.valid.addValidator(falseValidator)
+        model.onJoinPressed()
+        assertTrue(model.open.value)
+        verify(inverse = true) {
+            mockController.join(any())
+        }
+    }
+
+    @Test
+    fun `does nothing when invalid and text submitted`() {
+        model.valid.addValidator(falseValidator)
+        model.onTextAction()
+        assertTrue(model.open.value)
+        verify(inverse = true) {
+            mockController.join(any())
+        }
+    }
+
+}
