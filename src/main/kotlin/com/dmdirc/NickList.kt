2f9272128e55868e943b3fa40a716a6ef1cee6fe
Chris Smith <chris@chameth.com>
2019-03-07 21:03:56 +0000

    Use new ktirc features

--- src/main/kotlin/com/dmdirc/NickList.kt
+++ src/main/kotlin/com/dmdirc/NickList.kt
@@ -1,27 +1,23 @@
 package com.dmdirc
 
-import com.dmdirc.ktirc.events.*
+import com.dmdirc.ktirc.events.ChannelMembershipAdjustment
 
-class NickListModel(private val controller: ConnectionContract.Controller?) {
+class NickListModel {
 
     val users = mutableListOf<String>().observable()
 
-    fun handleEvent(event: IrcEvent) {
-        when (event) {
-            is ChannelJoined -> users.add(event.user.nickname)
-            is ChannelParted -> users.remove(event.user.nickname)
-            is ChannelQuit -> users.remove(event.user.nickname)
-            is ChannelUserKicked -> users.remove(event.victim)
-            is ChannelNickChanged -> {
-                users.remove(event.user.nickname)
-                users.add(event.newNick)
-            }
-            is ChannelNamesFinished -> {
-                users.clear()
-                controller?.let {
-                    users.addAll(it.getUsers(event.target).map { user -> user.nickname })
-                }
-            }
+    fun handleEvent(event: ChannelMembershipAdjustment) {
+        event.addedUser?.let {
+            users.add(it)
+        }
+
+        event.removedUser?.let {
+            users.remove(it)
+        }
+
+        event.replacedUsers?.let {
+            users.clear()
+            users.addAll(it)
         }
     }
 
