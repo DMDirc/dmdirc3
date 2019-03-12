fb52f29415848455fcce2dd4c6e92c59f15328b7
Chris Smith <chris@chameth.com>
2019-03-12 23:00:01 +0000

    Tidy up events a bit

--- src/main/kotlin/com/dmdirc/IrcEvents.kt
+++ src/main/kotlin/com/dmdirc/IrcEvents.kt
@@ -24,13 +24,13 @@ import com.dmdirc.ktirc.events.SourcedEvent
 import com.dmdirc.ktirc.events.TargetedEvent
 import com.dmdirc.ktirc.model.User
 import com.jukusoft.i18n.I
+import com.jukusoft.i18n.I.tr
 import java.time.format.DateTimeFormatter
 
 class IrcEventMapper(private val client: IrcClient) {
 
     fun flags(event: IrcEvent) = sequence {
         yield(if (event.channelEvent) ChannelEvent else ServerEvent)
-
         when (event) {
             is MessageReceived -> yield(Message)
             is ActionReceived -> yield(Action)
@@ -39,36 +39,47 @@ class IrcEventMapper(private val client: IrcClient) {
     }.toSet()
 
     fun displayableText(event: IrcEvent): Array<String>? = with(event) {
-        when (this) {
-            is ServerConnected -> arrayOf(I.tr("Connected"))
-            is ServerDisconnected -> arrayOf(I.tr("Disconnected"))
-            is ServerConnectionError -> arrayOf(I.tr("Error: %s - %s").format(error.translated(), details ?: ""))
-            is ChannelJoined -> arrayOf(I.tr("%s joined").format(formattedNickname))
-            is ChannelParted -> if (reason.isEmpty()) arrayOf(I.tr("%s left").format(formattedNickname))
-            else arrayOf(I.tr("%s left (%s)").format(formattedNickname, reason))
-            is ChannelQuit -> if (reason.isEmpty()) arrayOf(I.tr("%s quit").format(formattedNickname))
-            else arrayOf(I.tr("%s quit (%s)").format(formattedNickname, reason))
-            is ChannelNickChanged -> arrayOf(
-                I.tr("%s is now known as %s").format(formattedNickname, newNick.formattedNickname)
-            )
-            is ChannelTopicChanged -> arrayOf(I.tr("%s has changed the topic to: %s").format(formattedNickname, topic))
+        event.untranslatedText() ?: event.simpleText()?.let { arrayOf(it) } ?: when (this) {
+            is ChannelParted -> if (reason.isEmpty()) {
+                arrayOf(I.tr("%s left").format(formattedNickname))
+            } else {
+                arrayOf(I.tr("%s left (%s)").format(formattedNickname, reason))
+            }
+            is ChannelQuit -> if (reason.isEmpty()) {
+                arrayOf(I.tr("%s quit").format(formattedNickname))
+            } else {
+                arrayOf(I.tr("%s quit (%s)").format(formattedNickname, reason))
+            }
             is ChannelTopicDiscovered -> if (topic.isNullOrEmpty()) {
                 arrayOf(I.tr("there is no topic set"))
             } else {
                 arrayOf(I.tr("the topic is: %s").format(topic))
             }
-            is ChannelTopicMetadataDiscovered -> arrayOf(
-                I.tr("topic was set at %s on %s by %s").format(
-                    setTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")),
-                    setTime.format(DateTimeFormatter.ofPattern("cccc, d LLLL yyyy")),
-                    user.formattedNickname
-                )
-            )
-            is MessageReceived -> arrayOf(user.formattedNickname, message)
-            is NoticeReceived -> arrayOf(user.formattedNickname, message)
-            is ActionReceived -> arrayOf(user.formattedNickname, action)
+
             else -> null
         }
+    }
+
+    private fun IrcEvent.untranslatedText(): Array<String>? = when (this) {
+        is MessageReceived -> arrayOf(formattedNickname, message)
+        is NoticeReceived -> arrayOf(formattedNickname, message)
+        is ActionReceived -> arrayOf(formattedNickname, action)
+        else -> null
+    }
+
+    private fun IrcEvent.simpleText(): String? = when (this) {
+        is ServerConnected -> tr("Connected")
+        is ServerDisconnected -> tr("Disconnected")
+        is ServerConnectionError -> tr("Error: %s - %s").format(error.translated(), details ?: "")
+        is ChannelJoined -> tr("%s joined").format(formattedNickname)
+        is ChannelNickChanged -> tr("%s is now known as %s").format(formattedNickname, newNick.formattedNickname)
+        is ChannelTopicChanged -> tr("%s has changed the topic to: %s").format(formattedNickname, topic)
+        is ChannelTopicMetadataDiscovered -> tr("topic was set at %s on %s by %s").format(
+            setTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")),
+            setTime.format(DateTimeFormatter.ofPattern("cccc, d LLLL yyyy")),
+            user.formattedNickname
+        )
+        else -> null
     }
 
     private val IrcEvent.channelEvent: Boolean
