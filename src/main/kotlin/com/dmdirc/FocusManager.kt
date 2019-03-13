7d33ff3061d6cc5bb34383c851100a4abd88170b
Greg Holmes <git@greg.holmes.name>
2019-03-13 20:51:28 +0000

    Remove listener before adding new one

--- src/main/kotlin/com/dmdirc/FocusManager.kt
+++ src/main/kotlin/com/dmdirc/FocusManager.kt
@@ -1,25 +1,41 @@
 package com.dmdirc
 
 import javafx.beans.property.ObjectProperty
+import javafx.beans.value.ChangeListener
+import javafx.beans.value.ObservableValue
 import javafx.scene.Node
+import javafx.scene.control.TextField
 import javafx.stage.Stage
+
+class FocusListener(
+    private val uiNode: WindowUI,
+    private val dialogPane: ObjectProperty<Node>
+) : ChangeListener<Any> {
+    override fun changed(observable: ObservableValue<out Any>?, oldValue: Any?, newValue: Any?) {
+        if (newValue == false && dialogPane.value?.visibleProperty()?.value != true) {
+            uiNode.inputField.requestFocus()
+        }
+    }
+}
 
 class FocusManager(
     private val stage: Stage,
     private val controller: MainContract.Controller,
     private val dialogPane: ObjectProperty<Node>
 ) {
+    private var inputField: TextField? = null
+    private var inputFieldListener: ChangeListener<Any>? = null
+
     fun start() {
         stage.scene.focusOwnerProperty().addListener { _, _, _ ->
             val selectedWindowValue = controller.selectedWindow.value
             val uiNode = selectedWindowValue?.connection?.children?.get(selectedWindowValue.name.value)?.ui
             if (uiNode is WindowUI && dialogPane.value?.visibleProperty()?.value != true) {
+                inputField?.focusedProperty()?.removeListener(inputFieldListener)
+                inputField = uiNode.inputField
                 uiNode.inputField.requestFocus()
-                uiNode.inputField.focusedProperty().addListener { _, _, newValue ->
-                    if (newValue == false && dialogPane.value?.visibleProperty()?.value != true) {
-                        uiNode.inputField.requestFocus()
-                    }
-                }
+                inputFieldListener = FocusListener(uiNode, dialogPane)
+                uiNode.inputField.focusedProperty().addListener(inputFieldListener)
             }
         }
     }
