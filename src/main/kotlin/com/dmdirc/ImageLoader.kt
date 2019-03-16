af875ff4cd04f160ed0488366e2324a0d2feab85
Greg Holmes <git@greg.holmes.name>
2019-03-16 19:23:12 +0000

    Load images following redirects.
    This uses okhttp.
    This no longer has a progress bar.

--- src/main/kotlin/com/dmdirc/ImageLoader.kt
+++ src/main/kotlin/com/dmdirc/ImageLoader.kt
@@ -1,29 +1,47 @@
 package com.dmdirc
 
-import javafx.scene.control.ProgressBar
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
 import javafx.scene.layout.Pane
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import java.time.Duration
 
 class ImageLoader(url: String) : Pane() {
-    private val image: Image = Image(url, true)
-    private val progress = ProgressBar(0.toDouble())
     init {
-        children.add(progress)
-        image.progressProperty().addListener { _, _, newValue ->
-            progress.progressProperty().value = newValue.toDouble()
-            if (newValue.toDouble() >= 1.toDouble()) {
-                children.remove(progress)
-                children.add(ImageView(image).apply {
-                    fitWidth = 250.0
-                    fitHeight = 250.0
-                    isPreserveRatio = true
-                    isSmooth = true
-                })
+        val http = OkHttpClient().newBuilder()
+            .callTimeout(Duration.ofSeconds(2))
+            .build()
+        val request = Request.Builder().url(url).build()
+        http.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // NOOP
             }
-        }
-        image.errorProperty().addListener { _, _, _ ->
-            this@ImageLoader.children.remove(progress)
-        }
+
+            override fun onResponse(call: Call, response: Response) {
+                runLater {
+                    val stream = response.body()?.byteStream()
+                    if (response.isSuccessful && stream != null) {
+                        GlobalScope.launch {
+                            val image = Image(stream)
+                            runLater {
+                                children.add(ImageView(image).apply {
+                                    fitWidth = 250.0
+                                    fitHeight = 250.0
+                                    isPreserveRatio = true
+                                    isSmooth = true
+                                })
+                            }
+                        }
+                    }
+                }
+            }
+        })
     }
 }
